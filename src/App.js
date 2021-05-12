a8a4a633f716173bff564407e08516a2524126d6
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-05-12 14:55:02 +0200

    General layout and fetch data into view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setquestions] = useState(data);
+  return (
+  <main>
+  <div className='container'>
+    <h3>Questions et réponses à propos de la connexion</h3>
+    <section className='info'>
+      {questions.map((question) => {
+        return (
+          <SingleQuestion key={question.id} {...question}></SingleQuestion>
+        );
+      })}
+    </section>
+  </div>
+</main>);
 }
 
 export default App;
